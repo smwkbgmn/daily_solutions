ad7cffe940394a81720e286af54a0db50387281e
smwkbgmn <6moguai9@gmail.com>
2025-02-18 18:41:53 +0900

    0218

---
+++ 0218_1_12911.cpp
@@ -0,0 +1,26 @@
+/* 다음 큰 숫자 */
+
+#include <bitset>
+
+using namespace std;
+
+int solution(int n) {
+    bitset<24> bit(n);
+    
+    /* Move mask to first occurence of setd' bit */
+    bitset<24> mask(0b1);
+    while (!(bit & mask).any()) {
+        mask <<= 1;
+    }
+    
+    /* Move maks to first unsetd' bit while counting how many setd' bits are and turning these bits off */
+    int one = 0;
+    while ((bit & mask).any()) {
+        ++one;
+        bit ^= mask;
+        mask <<= 1;
+    }
+    
+    /* Return the decimal number by adding mask, which is holding where to set upper bit, and rest of setd' bits from right side as many as counted before */
+    return static_cast<int>(bitset<24>(bit | mask | bitset<24>((0b1 << one - 1) - 1)).to_ulong());
+}
---
+++ 0218_2_12945.cpp
@@ -0,0 +1,16 @@
+/* 피보나치 수 */
+
+using namespace std;
+
+int solution(int n) {
+    /* Problem ensures input n is above 1, no need to handle */
+    int pre_2 = 0, pre_1 = 1;
+    while (--n) {
+        int curnt = (pre_2 + pre_1) % 1234567;
+        
+        pre_2 = pre_1;
+        pre_1 = curnt;
+    }
+    
+    return pre_1;
+}
