08681c6bcbe7b82af8910283f6b72f87cd76956d
smwkbgmn <6moguai9@gmail.com>
2025-02-26 02:20:49 +0900

    0224, 0225

---
+++ 0225_1_87390.cpp
@@ -0,0 +1,51 @@
+/* n^2 배열 자르기 - 87390 */
+
+#include <vector>
+
+using namespace std;
+
+/* Increment */
+vector<int> solution(int n, long long left, long long right) {
+    vector<int> answer;
+    
+    int x_l = left % n, x_r = right % n;
+    int y_l = left / n, y_r = right / n;
+
+    for (auto y = y_l; y <= y_r; ++y) {
+
+		int x_min = (y == y_l)? x_l : 0;
+		int x_max = (y == y_r)? x_r : n - 1;
+        for (auto x = x_min; x <= x_max; ++x) {
+            answer.push_back(max(x, y) + 1);
+        }
+    }
+    return answer;
+}
+
+/* DIV and MOD */
+vector<int> solution(int n, long long left, long long right) {
+    vector<int> answer;
+    
+    for (auto i = left; i <= right; ++i) {
+		answer.push_back(max(i % n, i / n) + 1);
+	}
+    return answer;
+}
+
+
+/*
+i % n: x, i / n: y
+
+0, 0 = 0
+
+0, 1 = 1
+1, 0 = 1
+1, 1 = 1
+
+2, 0 = 2
+2, 1 = 2
+2, 2 = 2
+0, 2 = 2
+1, 2 = 2
+2, 2 = 2
+*/
