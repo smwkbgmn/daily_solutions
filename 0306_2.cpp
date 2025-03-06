c974dab2811b03cb1072d2e1ff88ecc78d89b0fa
smwkbgmn <6moguai9@gmail.com>
2025-03-06 22:01:23 +0900

    0306

---
+++ 0306_2.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+vector<int> solution(vector<int> numbers) {
+    vector<int> rst(numbers.size(), -1);
+    
+    stack<int> stk;
+    for (auto i = 0; i < numbers.size(); ++i) {
+        while (!stk.empty() && numbers[stk.top()] < numbers[i]) {
+            rst[stk.top()] = numbers[i];
+            stk.pop();
+        }
+        stk.push(i);
+    }
+    
+    return rst;
+}
