264451da1dd461d77e17317e8395f80da6486e31
smwkbgmn <6moguai9@gmail.com>
2025-03-22 23:14:14 +0900

    0321 0322

--- 0321_68645.cpp
+++ 0321_68645.cpp
@@ -27,3 +27,49 @@ vector<int> solution(int n) {
 
     return snail;
 }
+
+
+/* Version: 2d array + recursive */
+#include <algorithm>
+
+void fill(vector<vector<int>>& snail, int n, int d, int num) {
+    if (n > 3) {
+    	fill(snail, n - 3, d + 1, num + ((n - 1) * 3));
+    }
+
+	int r = d * 2;
+
+    for (auto i = 0; i < n - 1; ++i) {
+        snail[r++][d] = num++;
+    }
+    
+    for (auto i = 0; i < n; ++i) {
+        snail[r][d + i] = num++;
+    }
+	--num;
+
+    int size = snail[r].size();
+    for (auto i = 0; i < n - 1; ++i) {
+        snail[r--][size-- - d - 1] = num++;
+    }
+}
+
+vector<int> solution(int n) {
+    vector<vector<int>> snail;
+
+    for (auto r = 0; r < n; ++r) {
+        snail.push_back(vector<int>(r + 1));
+    }
+
+    fill(snail, n, 0, 1);
+
+	snail[0].resize(n * (n + 1) / 2);
+
+	int end = 1;
+	for (auto r = 1; r < n; ++r) {
+		move(snail[r].begin(), snail[r].end(), snail[0].begin() + end);
+		end += r + 1;
+	}
+    
+    return snail[0];
+}
