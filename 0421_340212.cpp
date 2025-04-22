f88c9e7b97f0799db09742f430908a542636b449
smwkbgmn <6moguai9@gmail.com>
2025-04-22 13:49:13 +0900

    0422

--- 0421_340212.cpp
+++ 0421_340212.cpp
@@ -6,29 +6,29 @@
 using namespace std;
 
 int solution(vector<int> puzzle, vector<int> duration, long long limit) {
-    int lv_min = 1, lv_max = *max_element(puzzle.begin(), puzzle.end());
+    int min = 1, max = *max_element(puzzle.begin(), puzzle.end());
     
     auto valid = [&](int level) -> bool {
         long long sum = 0;
         
         for (auto i = 0; i < puzzle.size(); ++i) {
-            int lv_diff = puzzle[i] - level;
-            sum += duration[i] + (lv_diff > 0? (duration[i] + duration[i - 1]) * lv_diff : 0);
+            int diff = puzzle[i] - level;
+            sum += duration[i] + (diff > 0? (duration[i] + duration[i - 1]) * diff : 0);
             
             if (sum > limit) return false;
         }
         return true;
     };
     
-    while (lv_min <= lv_max) {
-        int level = (lv_min + lv_max) / 2;
+    while (min <= max) {
+        int level = (min + max) / 2;
         
         if (valid(level)) {
-            lv_max = level - 1;
+            max = level - 1;
         } else {
-            lv_min = level + 1;
+            min = level + 1;
         }
     }
     
-    return lv_min;
+    return min;
 }
