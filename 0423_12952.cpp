ddfd19fc6af93262f3d555d6b815be10fc874f8a
smwkbgmn <6moguai9@gmail.com>
2025-04-23 19:13:45 +0900

    0423

---
+++ 0423_12952.cpp
@@ -0,0 +1,41 @@
+/* N-Queen - 12952 */
+
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int n) {
+    auto valid = [&](const vector<int>& queen, int c) -> bool {
+        /* Validate Horizonal(column) and Vertical(row) */ 
+        if (!queen.empty() && abs(queen.back() - c) < 2 ||
+            find(queen.begin(), queen.end(), c) != queen.end()) {
+            return false;
+        }
+        
+        /* Validate Diagonal */
+        for (size_t i = 0, pos = queen.size(); i < queen.size(); ++i) {
+            if (pos - i == abs(queen[i] - c)) {
+                return false;
+            }
+        }
+        return true;
+    };
+
+    auto dfs = [&](auto& self, int r, vector<int>& queen) -> int {
+        if (r == n) return 1;
+        
+        int sum = 0;
+        for (auto c = 0; c < n; ++c) {
+            if (!valid(queen, c)) continue;
+            
+            queen.push_back(c);
+            sum += self(self, r + 1, queen);
+            queen.erase(queen.begin() + r, queen.end());
+        }
+        return sum;
+    };
+    
+    vector<int> holder;
+    return dfs(dfs, 0, holder);
+}
