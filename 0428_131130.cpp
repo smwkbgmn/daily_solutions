801153f7d7bdee7014df4c46b5cded3324f348e4
smwkbgmn <6moguai9@gmail.com>
2025-04-28 16:13:54 +0900

    0428

---
+++ 0428_131130.cpp
@@ -0,0 +1,25 @@
+/* 혼자 놀기의 달인 - 131130 */
+
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> cards) {
+    vector<int> counts;
+	
+    for (auto i = 0; i < cards.size(); ++i) {
+        if (cards[i] == 0) continue;
+        
+        int count = 0;
+        for (auto curr = i, next = cards[i] - 1; cards[curr] > 0; curr = next, next = cards[curr] - 1) {
+            cards[curr] = 0;
+            ++count;
+        }
+        counts.push_back(count);
+    }
+    
+    sort(counts.begin(), counts.end(), greater<int>());
+    
+    return (counts.size() == 1)? 0 : counts[0] * counts[1];
+}
